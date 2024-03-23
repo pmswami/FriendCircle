f394717c3f1c2af997333c8897f822fd2bac0477
unknown <usern5949@gmail.com>
2024-03-23 22:28:22 +0530

    changed jinja temaplates

--- FriendCircle/base/views.py
+++ FriendCircle/base/views.py
@@ -22,5 +22,10 @@ def home(request):
     context = {"rooms":rooms}
     return render(request, "base/home.html", context)
 
-def room(request):
-    return render(request, "base/room.html")
+def room(request, pk):
+    room=None
+    for item in rooms:
+        if item["id"]==int(pk):
+            room=item
+    context={"room": room}
+    return render(request, "base/room.html", context)
