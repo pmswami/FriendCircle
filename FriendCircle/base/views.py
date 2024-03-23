ba640c6ad78921cdd6f684104c88bd8f26799cd1
unknown <usern5949@gmail.com>
2024-03-23 22:11:48 +0530

    created new app, added templates

---
+++ FriendCircle/base/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+rooms = [
+    {
+        "id":1,
+        "name":"Lets learn Python"
+    },
+    {
+        "id":2,
+        "name":"Design with me"
+    },
+    {
+        "id":3,
+        "name":"Frontend Developers"
+    }
+]
+
+def home(request):
+    context = {"rooms":rooms}
+    return render(request, "base/home.html", context)
+
+def room(request):
+    return render(request, "base/room.html")
