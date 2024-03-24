27122de7ba749e3dcf3c7c85f37e57ad81359680
unknown <usern5949@gmail.com>
2024-03-24 18:45:15 +0530

    Added models, create-room URL

--- FriendCircle/base/admin.py
+++ FriendCircle/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Room, Topic, Message
+
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
--- FriendCircle/base/views.py
+++ FriendCircle/base/views.py
@@ -1,31 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-    {
-        "id":1,
-        "name":"Lets learn Python"
-    },
-    {
-        "id":2,
-        "name":"Design with me"
-    },
-    {
-        "id":3,
-        "name":"Frontend Developers"
-    }
-]
+# rooms = [
+#     {
+#         "id":1,
+#         "name":"Lets learn Python"
+#     },
+#     {
+#         "id":2,
+#         "name":"Design with me"
+#     },
+#     {
+#         "id":3,
+#         "name":"Frontend Developers"
+#     }
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms":rooms}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
-    room=None
-    for item in rooms:
-        if item["id"]==int(pk):
-            room=item
-    context={"room": room}
+    rooms = rooms = Room.objects.get(id=pk)
+    context={"room": rooms}
     return render(request, "base/room.html", context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, 'base/room_form.html', context)
