f39561dd3577052a00bbcc8f27384c740c3e01b7
unknown <usern5949@gmail.com>
2024-03-25 12:56:17 +0530

    Added search functionality

--- FriendCircle/base/views.py
+++ FriendCircle/base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 # Create your views here.
 
@@ -21,8 +22,17 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms":rooms}
+    print(request)
+    q = request.GET.get("q") if request.GET.get("q") else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    # rooms = Room.objects.all()
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {"rooms":rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
